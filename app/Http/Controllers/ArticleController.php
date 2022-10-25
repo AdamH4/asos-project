3def0c22ab98659b4b40df46a119d0948e26c8b9
Adam Harnusek <adam.harnusek@gmail.com>
2022-10-25 14:24:27 +0200

    add Angolia state to controller :monkey_face:

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -2,21 +2,25 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\AlgoliaArticle;
 use App\Models\Article;
 use Illuminate\Http\Request;
-use Elasticsearch\ClientBuilder;
 
 class ArticleController extends Controller
 {
     public function search(Request $request)
     {
 
-        //Elasticsearch
+        // Elasticsearch
         $articles = Article::search("$request->search", function ($client, $body) {
-            //$body->setSize(10);
             return $client->search(['index' => 'articles', 'body' => $body->toArray()]);
         })->get();
 
+        $articles = Article::search("$request->search", function ($client, $body) {
+            return $client->search(['index' => 'articles', 'body' => $body->toArray()]);
+        })->get();
+
+        $articles = AlgoliaArticle::search("$request->search")->get();
 
         return view('welcome', [
             'articles' => $articles,
